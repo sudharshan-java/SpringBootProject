120687ae26891f272ba6f8e68c64a74124d003e0
Sudharshan <Sudharshan@LAPTOP-NI0G8QDA>
2020-03-07 09:20:19 +0530

    getContact() method is Added in Contact Service Class.

--- SpringBootApplication-Basic/src/main/java/com/nt/yadav/service/ContactService.java
+++ SpringBootApplication-Basic/src/main/java/com/nt/yadav/service/ContactService.java
@@ -2,6 +2,9 @@ package com.nt.yadav.service;
 
 public class ContactService {
 
-	
+	public String getContacts() {
+		System.out.println("getContact Method is Executed");
+		return "Contact List Saved";
+	}
 	
 }
